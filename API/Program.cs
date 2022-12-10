aa9012c790ff3a58059290b2dc7064daea161bd5
ed <allgoodata@outlook.com>
2022-12-10 11:08:35 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -3,16 +3,19 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Services
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
+builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
+// App builder
 var app = builder.Build();
 
+// Middlewares
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.MapControllers();
 
 app.Run();
