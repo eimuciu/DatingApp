d63874b0a3eaa894c9db89f295ae90847eb4a59f
ed <allgoodata@outlook.com>
2022-12-16 13:29:32 +0200

    S6L65 Adding the nav links

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -8,40 +8,16 @@ import { AccountService } from '../_services/account.service';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
-// *1.1 All commented out fields are commented because observables will be used directly in ng html file provided by ng which will automatically unsubscribe observable upon completion
-
-// *1.2 Removing the line so accountService can be used directly inside html
 export class NavComponent implements OnInit {
   model: any = {};
-  // *1.2
-  // currentUser$: Observable<User | null> = of(null);
-  //*1.1
-  // loggedIn = false;
-
-  //*1.2 chancing from private to public so accountService can be used directly inside html
   constructor(public accountService: AccountService) {}
 
-  ngOnInit(): void {
-    //*1.1
-    // this.getCurrentUser();
-    //*1.2 Removing the line so accountService can be used directly inside html
-    // this.currentUser$ = this.accountService.currentUser$;
-  }
-
-  // *1.1
-  // getCurrentUser() {
-  //   this.accountService.currentUser$.subscribe({
-  //     next: (user) => (this.loggedIn = !!user),
-  //     error: (error) => console.log(error),
-  //   });
-  // }
+  ngOnInit(): void {}
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: (response) => {
         console.log(response);
-        //*1.1
-        // this.loggedIn = true;
       },
       error: (error) => console.log(error),
     });
@@ -49,7 +25,5 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
-    //*1.1
-    // this.loggedIn = false;
   }
 }
