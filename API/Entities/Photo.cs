a96ded1d0352feb8e9c524c536882d6c35124e7c
ed <allgoodata@outlook.com>
2023-01-09 15:36:34 +0200

    S8L86 Entity Framework relationships

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,12 +1,17 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace API.Entities
 {
+    [Table("Photos")]
     public class Photo
     {
         public int Id { get; set; }
         public string Url { get; set; }
         public bool IsMain { get; set; }
         public string PublicId { get; set; }
+
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
     }
 }
