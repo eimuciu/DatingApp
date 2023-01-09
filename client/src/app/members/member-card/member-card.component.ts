394f4640fdd0067b109da4201e47f4283a86be7d
ed <allgoodata@outlook.com>
2023-01-09 21:21:40 +0200

    S9L105 Adding some style to the cards

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
   styleUrls: ['./member-card.component.css'],
+  // encapsulation: ViewEncapsulation.Emulated, --> This is a default setting that uses angular naming to specify styling only for this component
+  // encapsulation: ViewEncapsulation.None --> This encapsulation sets a global styling
 })
 export class MemberCardComponent implements OnInit {
   @Input() member: Member | undefined;
