a5e57dc9d43940479951639c8f93cd85fda7383c
ed <allgoodata@outlook.com>
2023-01-09 17:20:58 +0200

    Adding a DTO for Members

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -10,8 +10,8 @@ namespace API.Entities
         public byte[] PasswordSalt { get; set; }
         public DateOnly DateOfBirth { get; set; }
         public string KnownAs { get; set; }
-        public DateTime Created { get; set; }
-        public DateTime LastActive { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
         public string Gender { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
