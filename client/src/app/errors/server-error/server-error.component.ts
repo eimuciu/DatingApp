098730c7201d701ff5a8ad3cccadc10735071fee
ed <allgoodata@outlook.com>
2023-01-09 14:13:58 +0200

    End of section 7

--- client/src/app/errors/server-error/server-error.component.ts
+++ client/src/app/errors/server-error/server-error.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-server-error',
   templateUrl: './server-error.component.html',
-  styleUrls: ['./server-error.component.css']
+  styleUrls: ['./server-error.component.css'],
 })
 export class ServerErrorComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  error: any;
+  //We only get a chance to see what is in the router state in the constructor. Once it passes constructor and goes to ngOnInit it is already too late to access router state
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    this.error = navigation?.extras?.state?.['error'];
   }
 
+  ngOnInit(): void {}
 }
