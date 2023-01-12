5488fa47d75735183ddae7dc187fd1d4ad97a606
ed <allgoodata@outlook.com>
2023-01-12 18:08:11 +0200

    S12L147 Adding a reusable date input

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -6,6 +6,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { FileUploadModule } from 'ng2-file-upload';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 @NgModule({
   declarations: [],
@@ -21,6 +22,7 @@ import { FileUploadModule } from 'ng2-file-upload';
       type: 'line-scale-party',
     }),
     FileUploadModule,
+    BsDatepickerModule.forRoot(),
   ],
   exports: [
     BsDropdownModule,
@@ -29,6 +31,7 @@ import { FileUploadModule } from 'ng2-file-upload';
     NgxGalleryModule,
     NgxSpinnerModule,
     FileUploadModule,
+    BsDatepickerModule
   ],
 })
 export class SharedModule {}
