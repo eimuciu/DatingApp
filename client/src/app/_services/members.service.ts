cfa516dd326e4107cc60691a695be05b5fd61e59
ed <allgoodata@outlook.com>
2023-01-15 17:09:18 +0200

    S13L166 Restoring the chaching for members

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -13,10 +13,15 @@ export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
   // paginatedResult: PaginatedResult<Member[]> = new PaginatedResult<Member[]>();
+  memberCache = new Map();
 
   constructor(private http: HttpClient) {}
 
   getMembers(userParams: UserParams) {
+    const response = this.memberCache.get(Object.values(userParams).join('-'));
+
+    if (response) return of(response);
+
     let params = this.getPaginationHeaders(
       userParams.pageNumber,
       userParams.pageSize
@@ -27,7 +32,15 @@ export class MembersService {
     params = params.append('gender', userParams.gender);
     params = params.append('orderBy', userParams.orderBy);
 
-    return this.getPaginatedResults<Member[]>(this.baseUrl + 'users', params);
+    return this.getPaginatedResults<Member[]>(
+      this.baseUrl + 'users',
+      params
+    ).pipe(
+      map((response) => {
+        this.memberCache.set(Object.values(userParams).join('-'), response);
+        return response;
+      })
+    );
   }
 
   private getPaginatedResults<T>(url: string, params: HttpParams) {
