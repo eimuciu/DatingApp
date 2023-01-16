fa4dae7eced4ca4d178fbce69257291f247d235d
ed <allgoodata@outlook.com>
2023-01-16 12:49:45 +0200

    S15L182 Setting up the message repository

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -21,5 +21,7 @@ namespace API.Entities
         public List<Photo> Photos { get; set; } = new(); //new() is a shorter version of new List<Photo>() 
         public List<UserLike> LikedByUsers { get; set; }
         public List<UserLike> LikedUsers { get; set; }
+        public List<Message> MessagesSent { get; set; }
+        public List<Message> MessagesReceived { get; set; }
     }
 }
