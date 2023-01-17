2e409038b56a5384a1538a2f37c97ef466dc4604
ed <allgoodata@outlook.com>
2023-01-17 13:12:13 +0200

    S15L193 Using route resolvers

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -18,7 +18,7 @@ import { MessageService } from 'src/app/_services/message.service';
 })
 export class MemberDetailComponent implements OnInit {
   @ViewChild('memberTabs', { static: true }) memberTabs?: TabsetComponent;
-  member: Member | undefined;
+  member: Member = {} as Member;
   galleryOptions: NgxGalleryOptions[] = [];
   galleryImages: NgxGalleryImage[] = [];
   activeTab?: TabDirective;
@@ -31,7 +31,9 @@ export class MemberDetailComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.loadMember();
+    this.route.data.subscribe({
+      next: (data) => (this.member = data['member']),
+    });
 
     this.route.queryParams.subscribe({
       next: (params) => {
@@ -49,6 +51,8 @@ export class MemberDetailComponent implements OnInit {
         preview: false,
       },
     ];
+
+    this.galleryImages = this.getImages();
   }
 
   getImages() {
@@ -68,16 +72,6 @@ export class MemberDetailComponent implements OnInit {
     if (this.memberTabs) {
       this.memberTabs.tabs.find((x) => x.heading === heading)!.active = true;
     }
-  }
-
-  loadMember() {
-    var username = this.route.snapshot.paramMap.get('username');
-    if (!username) return;
-    this.memberService.getMember(username).subscribe({
-      next: (member) => {
-        (this.member = member), (this.galleryImages = this.getImages());
-      },
-    });
   }
 
   loadMessages() {
