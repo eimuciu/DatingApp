67b13fd8359fdf0cfcedba95610734bf4adf2c8c
ed <allgoodata@outlook.com>
2023-01-17 16:43:17 +0200

    S16L203 Refactoring and adding a new migration

--- API/Program.cs
+++ API/Program.cs
@@ -32,7 +32,7 @@ try
 {
     var context = services.GetRequiredService<DataContext>();
     await context.Database.MigrateAsync();
-    await Seed.SeedUsers(context);
+    // await Seed.SeedUsers(context);
 }
 catch (Exception ex)
 {
