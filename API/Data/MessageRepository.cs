d63c76ac9b951a5f069ff9579aa2fc4314e9ad26
ed <allgoodata@outlook.com>
2023-01-17 13:30:53 +0200

    S15L194 Sending message

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -61,7 +61,7 @@ namespace API.Data
                     m => m.RecipientUsername == currentUserName && m.SenderUsername == recipientUserName ||
                     m.RecipientUsername == recipientUserName &&
                     m.SenderUsername == currentUserName
-                ).OrderByDescending(m => m.MessageSent)
+                ).OrderBy(m => m.MessageSent)
                 .ToListAsync();
 
             var unreadMessages = messages.Where(m => m.DateRead == null && m.RecipientUsername == currentUserName).ToList();
