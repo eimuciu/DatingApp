f17a23020b45b62715308389972b046e1168b215
ed <allgoodata@outlook.com>
2023-01-23 17:53:41 +0200

    S19L249 Adding a fallback controller

--- API/Program.cs
+++ API/Program.cs
@@ -36,6 +36,7 @@ app.UseStaticFiles(); // be default this will look for wwwroot and serve the con
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
+app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
