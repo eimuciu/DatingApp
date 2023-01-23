c562c20156b7050e13eb63bf7f7bc0b7013034bc
ed <allgoodata@outlook.com>
2023-01-23 12:25:56 +0200

    S18L241 Optimizing queries part two

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -27,12 +27,12 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<PagedList<MemberDto>>> GetUsers([FromQuery] UserParams userParams)
         {
-            var currentUser = await _uow.UserRepository.GetUserByUsernameAsync(User.GetUserName());
-            userParams.CurrentUsername = currentUser.UserName;
+            var gender = await _uow.UserRepository.GetUserGender(User.GetUserName());
+            userParams.CurrentUsername = User.GetUserName();
 
             if (string.IsNullOrEmpty(userParams.Gender))
             {
-                userParams.Gender = currentUser.Gender == "male" ? "female" : "male";
+                userParams.Gender = gender == "male" ? "female" : "male";
             }
 
             var users = await _uow.UserRepository.GetMembersAsync(userParams);
